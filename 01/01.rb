e163eaaf43c135d0e219d1cab6ec5bd675f6296c
Quinn Daley <quinn@fishpercolator.co.uk>
2025-12-01 07:53:29 +0000

    Solution to day 1 - harder than it looked\!

---
+++ 01/01.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+class Dial
+  attr_reader :acc, :pass, :pass_click
+
+  def initialize
+    @acc = 50
+    @pass = 0
+    @pass_click = 0
+  end
+
+  def turn!(n)
+    @pass_click += clicks(acc,n)
+    @acc += (n%100)
+    @acc %= 100
+    @pass += 1 if acc.zero?
+  end
+
+  # How many times do we pass 0 if we apply n to acc?
+  def clicks(acc,n)
+    res = acc + n
+    rotations = (res.abs / 100)
+    if (acc.zero? && res < 0) || (res > 0)
+      rotations
+    else
+      # If we didn't start at zero and we end up negative, we passed zero
+      # during the first rotation, so add 1
+      rotations + 1
+    end
+  end
+
+  # Returns the numeric value of the given instruction
+  def inst_n(inst)
+    /([LR])(\d+)/.match(inst).captures.then {|d,n| n.to_i * (d=='L'?-1:1)}
+  end
+
+  def load!(f)
+    f.each {turn! inst_n(_1)}
+  end
+end
+
+d = Dial.new
+d.load!(ARGF)
+puts d.pass
+puts d.pass_click
