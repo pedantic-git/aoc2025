35a2e7328a3fa31a2a6f92b0390d94793f66133f
Quinn Daley <quinn@fishpercolator.co.uk>
2025-12-02 07:10:44 +0000

    Use Ruby 3.4's it

--- 02/02.rb
+++ 02/02.rb
@@ -3,11 +3,10 @@
 acc1, acc2 = 0, 0
 
 ARGF.read.scan(/(\d+)-(\d+)/) do |l,r|
-  (l.to_i).upto(r.to_i) do |n|
-    acc1 += n if /\A(.+)\1\Z/ =~ n.to_s
-    acc2 += n if /\A(.+)\1+\Z/ =~ n.to_s
+  (l.to_i).upto(r.to_i) do
+    acc1 += it if /\A(.+)\1\Z/ =~ it.to_s
+    acc2 += it if /\A(.+)\1+\Z/ =~ it.to_s
   end
 end
 
 puts acc1, acc2
-
