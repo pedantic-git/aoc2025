a709fe85ea55de01da915d28180dbaa2295c6f97
Quinn Daley <quinn@fishpercolator.co.uk>
2025-12-05 07:36:04 +0000

    Solution to part 2

--- 05/05.rb
+++ 05/05.rb
@@ -11,11 +11,23 @@ class Ims
     until /^$/ =~ (l=io.readline)
       add_range! l
     end
+    consolidate!
   end
 
   def add_range!(s)
     r = s.split('-').then {_1.to_i .. _2.to_i}
-    @ranges << r
+    ranges << r
+  end
+
+  # Consolidate any ranges that overlap... could be more elegant I reckon
+  def consolidate!
+    ranges.dup.each do |r|
+      if overs = ranges.find_all {(it != r) && it.overlap?(r)}
+        to_delete = overs << r
+        to_delete.each {ranges.delete it}
+        ranges << (to_delete.map(&:min).min .. to_delete.map(&:max).max)
+      end
+    end
   end
 
   def fresh?(n)
@@ -26,7 +38,12 @@ class Ims
     io.count {fresh? it}
   end
 
+  def n_possible
+    ranges.sum(&:count)
+  end
+
 end
 
 i = Ims.new(ARGF)
 p i.n_fresh
+p i.n_possible
