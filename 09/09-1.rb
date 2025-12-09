472d126066f74f1cbd6bb972a60d6a434af1fea7
Quinn Daley <quinn@fishpercolator.co.uk>
2025-12-09 07:45:57 +0000

    Part 1 - if in doubt, brute force it

--- 09/09-1.rb
+++ 09/09-1.rb
@@ -1,14 +1,12 @@
 #!/usr/bin/env ruby
+require 'matrix'
 
-coords = ARGF.readlines.map {it.split(',').map(&:to_i)}
+# Find the size of a rectangle between vectors c1 and c2
+def rect(c1,c2)
+  x = [c1[0], c2[0]].sort.reverse.reduce(:-) + 1
+  y = [c1[1], c2[1]].sort.reverse.reduce(:-) + 1
+  x * y
+end
 
-# Find the extremes
-nw = coords.min_by { [_1, _2] }
-ne = coords.min_by { [-_1, _2] }
-sw = coords.min_by { [_1, -_2] }
-se = coords.min_by { [-_1, -_2] }
-
-# There are more than 2 possible rectangles if the points are weirdly distributed
-# but they still involve extremes, right?
-
-p [nw,ne,sw,se].combination(2).map { (it[1][0]-it[0][0]+1) * (it[1][1]-it[1][0]+1) }.max
+coords = ARGF.readlines.map {it.split(',').map(&:to_i).then {Vector[*it]}}
+p coords.combination(2).map { rect _1, _2 }.max
